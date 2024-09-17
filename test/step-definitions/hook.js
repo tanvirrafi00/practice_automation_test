e1977fa5c7a3368900d0fa2464099dffac89cb53
Tanvir Rafi <tanvirrafi148@gmail.com>
2024-09-17 19:54:24 +0600

    hooks.js changes

--- test/step-definitions/hook.js
+++ test/step-definitions/hook.js
@@ -1,6 +1,5 @@
 import { Before } from "@wdio/cucumber-framework";
 import Browser from "../../framework/browser/Browser.js";
 Before({ tagExpression: "@check-out" }, async () => {
-  console.log("i am running...................");
   await Browser.clearCookies();
 });
