c356b5bc27bbdea87ebd625fae222e8816016528
Tanvir Rafi <tanvirrafi148@gmail.com>
2024-09-18 11:41:40 +0600

    tag remove from the before hook

--- test/hooks/hook.js
+++ test/hooks/hook.js
@@ -1,5 +1,5 @@
 import { Before } from "@wdio/cucumber-framework";
 import Browser from "../../framework/browser/Browser.js";
-Before({ tagExpression: "@check-out" }, async () => {
+Before(async () => {
   await Browser.clearCookies();
 });
