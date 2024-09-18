a49fb760dca10ebf463d071acf4ca3de28580a4a
Tanvir Rafi <tanvirrafi148@gmail.com>
2024-09-18 11:43:10 +0600

    step implemented

--- test/step-definitions/checkOutPageStep.js
+++ test/step-definitions/checkOutPageStep.js
@@ -1,7 +1,26 @@
 import { Given, When, Then } from "@wdio/cucumber-framework";
 import { assert } from "chai";
 import checkOutPage from "../page-objects/CheckOutPage.js";
+import data from "../../test data/data.json" assert { type: "json" };
+import message from "../../test data/message.json" assert { type: "json" };
+import CheckOutPage from "../page-objects/CheckOutPage.js";
 
 Then(/^I should be on the check out page$/, async () => {
   assert.isTrue(await checkOutPage.isPageOpened());
+});
+
+When(/^I enter Coupon code in coupon code field$/, async () => {
+  await CheckOutPage.addCoupon(data.coupon);
+});
+
+When(/^I click on the apply coupon button$/, async () => {
+  await checkOutPage.clickOnApplyCoupon();
+});
+
+Then(/^I should see coupon code added message$/, async () => {
+  assert.include(await checkOutPage.getCouponAddedMessage(), message.couponCodeAdded, "Coupon code added message not showing or not matching");
+});
+
+Then(/^I should see "(.*)" basket totals table$/, async (couponAmmountMessage) => {
+  assert.include(await checkOutPage.getCouponAmmount(), couponAmmountMessage, "Coupon ammount message not showing in the basket table");
 });
