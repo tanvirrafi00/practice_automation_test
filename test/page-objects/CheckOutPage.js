047aee18a200c82b580cc45ce3db5a7d98f1035f
Tanvir Rafi <tanvirrafi148@gmail.com>
2024-09-18 11:42:45 +0600

    New aelement added in checkout page

--- test/page-objects/CheckOutPage.js
+++ test/page-objects/CheckOutPage.js
@@ -1,9 +1,29 @@
-import { Label } from "../../framework/elements/index.js";
+import { Input, Label } from "../../framework/elements/index.js";
 import BasePage from "../../framework/page/BasePage.js";
 
 class CheckOutPage extends BasePage {
   constructor() {
     super(new Label(`//table[@class="shop_table shop_table_responsive cart"]`), "Check out page");
+    this.couponCodeField = new Input(`//input[@id="coupon_code"]`, "Coupon code field");
+    this.applyCouponButton = new Input(`//input[@name="apply_coupon"]`, "Apply coupon button");
+    this.couponAddedMessage = new Label(`//div[@class="woocommerce-message"]`, "Coupon added message");
+    this.couponAmmount = new Label(`//td[@data-title='Coupon: krishnasakinala']`, "coupon ammount");
+  }
+
+  async addCoupon(coupon) {
+    await this.couponCodeField.typeText(coupon);
+  }
+
+  async clickOnApplyCoupon() {
+    await this.applyCouponButton.click();
+  }
+
+  async getCouponAddedMessage() {
+    return await this.couponAddedMessage.getText();
+  }
+
+  async getCouponAmmount() {
+    return await this.couponAmmount.getText();
   }
 }
 
