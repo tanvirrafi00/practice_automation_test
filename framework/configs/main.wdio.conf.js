fc1e1edf0b9be3b70e3384af697e1f4f42bb986d
Tanvir Rafi <tanvirrafi148@gmail.com>
2024-09-19 23:14:00 +0600

    timoute updated

--- framework/configs/main.wdio.conf.js
+++ framework/configs/main.wdio.conf.js
@@ -10,7 +10,7 @@ export const mainConfig = {
   logLevel: "warn",
   bail: 0,
   waitforTimeout: 0,
-  connectionRetryTimeout: 120000,
+  connectionRetryTimeout: 200000,
   connectionRetryCount: 3,
   reporters: ["spec"],
 
