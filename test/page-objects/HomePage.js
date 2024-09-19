ff3584fd0f9c1ba5238013cea68072c251d9431e
Tanvir Rafi <tanvirrafi148@gmail.com>
2024-09-19 23:16:17 +0600

    my account link changed

--- test/page-objects/HomePage.js
+++ test/page-objects/HomePage.js
@@ -8,6 +8,7 @@ class HomePage extends BasePage {
     super(new Label(PreciseTextLocator(`new arrivals`)), "Home Page");
 
     this.shopLink = new Label(`//a[contains(text(),"Shop")]`, "Shop link");
+    this.myAccountLink = new Label(`//*[text()="My Account"]`, "my account link");
     this.allSliders = new ElementsList(Label, `//div[@data-gutter="gutter-default"]`, "all sliders");
     this.allArrivals = new ElementsList(Label, `//a[@class="woocommerce-LoopProduct-link"]`, "All arrivals");
     this.firstArrival = new Label(`//a[@href='https://practice.automationtesting.in/product/selenium-ruby/']`, "first arrival");
@@ -23,11 +24,17 @@ class HomePage extends BasePage {
 
   async clickOnShopMenu() {
     await this.shopLink.click();
-    console.log(await this.shopLink);
   }
 
   async clickOnFirstArrivalImage() {
     await this.firstArrival.click();
+  }
+
+  async clickOnMyaccont() {
+    console.log("I am going to click on my account........................");
+    await this.myAccountLink.state().waitForClickable();
+    await this.myAccountLink.click();
+    console.log("clicked on the my account................................");
   }
 }
 
